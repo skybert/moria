eeffdc3fa980c090780e59bc3dcabec86e7c0692
Torstein Krause Johansen <tkj@conduct.no>
2013-03-06 17:02:39 +0100

    - added run and print

---
+++ src/common-lib/indispensable.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+# by torstein.k.johansen@gmail.com
+
+function run() {
+  if [[ -n "$log_file" ]]; then
+    "$@" 1>> $log_file 2>> $log_file
+  else
+    "$@"
+  fi
+
+  if [ $? -gt 0 ]; then
+    print "The command <$@> failed :-("
+    exit 1
+  fi
+}
+
+function print() {
+  echo "$@"
+}
---
+++ test/common-lib/indispensable.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+# by torstein.k.johansen@gmail.com
+
+source ../../src/common-lib/indispensable.sh
+
+log_file=/tmp/hei.log
+
+run lsd
+print hei
