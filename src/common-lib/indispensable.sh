9a50e54c0b9df813fdb3311061513e6e608f9b96
Torstein Krause Johansen <tkj@conduct.no>
2013-07-05 10:01:13 +0200

    - added get_human_time to indispensable.sh

--- src/common-lib/indispensable.sh
+++ src/common-lib/indispensable.sh
@@ -37,3 +37,17 @@ function make_dir() {
     fi
   done
 }
+
+function get_human_time() {
+  local seconds_worked=$1
+
+  local days=$(( seconds_worked / ( 60 * 60 * 24 ) ))
+  local seconds_left=$(( seconds_worked - ( $days * 60 * 60 * 24 ) ))
+  local hours=$(( seconds_left / ( 60 * 60 ) ))
+  local seconds_left=$(( seconds_left - ( $hours * 60 * 60 ) ))
+  local minutes=$(( seconds_left / 60 ))
+  local seconds_left=$(( seconds_left - $minutes * 60 ))
+
+  echo "${hours}h ${minutes}m ${seconds_left}s"
+}
+
