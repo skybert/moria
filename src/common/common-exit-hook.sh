ce600fc3d2b6aa2855e0c0f9c9c6b00dcf63f142
Torstein Krause Johansen <torstein@escenic.com>
2016-01-08 14:32:33 +0100

    Made user feedback smoother

--- src/common/common-exit-hook.sh
+++ src/common/common-exit-hook.sh
@@ -3,12 +3,12 @@ start_time=$(date +%s)
 my_exit_hook() {
   local exit_status=$?
   local end_time=$(date +%s)
-  local it_took=$(echo "It took" $(( end_time - start_time )) "seconds.")
+  local it_took=$(echo "It took" $(( end_time - start_time )) "seconds")
 
   if [ ${exit_status} -eq 0 ]; then
     echo $(basename $0)  $(green SUCCESS ) ${it_took}
   else
-    echo $(basename $0)  $(red FAILED ) ${it_took} ", last messages in ${log_file}:"
+    echo $(basename $0)  $(red FAILED ) ${it_took}", last messages in ${log_file}:"
     tail -10 ${log_file}
   fi
 
