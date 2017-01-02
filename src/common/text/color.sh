c011abf390b24fa530d80ec0ea994594a8498604
Torstein Krause Johansen <torstein@escenic.com>
2017-01-02 14:17:22 +0100

    Support for palantir [--format <html>]
    - some logic in color to not output shell codes if format is HTML,
      default format is nothing

--- src/common/text/color.sh
+++ src/common/text/color.sh
@@ -6,26 +6,44 @@
 ##
 ## $@: input string
 function red() {
-  echo -e "\E[37;31m\033[1m${@}\033[0m"
+  if [[ "${output_format-""}" == "html" ]]; then
+    printf "%s" "$*"
+  else
+    echo -e "\E[37;31m\033[1m${@}\033[0m"
+  fi
+
 }
 
 ## Returns the inputted string(s) as green
 ##
 ## $@: input string
 function green() {
-  echo -e "\E[37;32m\033[1m${@}\033[0m"
+  if [[ "${output_format-""}" == "html" ]]; then
+    printf "%s" "$*"
+  else
+    echo -e "\E[37;32m\033[1m${@}\033[0m"
+  fi
 }
 
 ## Returns the inputted string(s) as yellow
 ##
 ## $@: input string
 function yellow() {
-  echo -e "\E[37;33m\033[1m${@}\033[0m"
+  if [[ "${output_format-""}" == "html" ]]; then
+    printf "%s" "$*"
+  else
+    echo -e "\E[37;33m\033[1m${@}\033[0m"
+  fi
 }
 
 ## Returns the inputted string(s) as blue
 ##
 ## $@: input string
 function blue() {
-  echo -e "\E[37;34m\033[1m${@}\033[0m";
+  if [[ "${output_format-""}" == "html" ]]; then
+    printf "%s" "$*"
+  else
+    echo -e "\E[37;34m\033[1m${@}\033[0m";
+  fi
+
 }
